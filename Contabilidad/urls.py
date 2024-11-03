96d245b8d0db672f22f8b439f1c2aeb619b37968
RC22009ues <hec159357@gmail.com>
2024-11-03 17:17:52 -0600

    login

--- Contabilidad/urls.py
+++ Contabilidad/urls.py
@@ -3,11 +3,14 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index),
-    path('catalogoCuenta/', views.catalogoCuenta),
-    path('transacciones/', views.transaccion),
-    path('balanceComprobacion/', views.balanceComprobacion),
-    path('estadoResultados/', views.estadoResultados),
-    path('estadoCapital/', views.estadoCapital),
-    path('balanceGeneral/', views.balanceGeneral),
+    path('', views.signin, name='signin'),
+    path('signin/', views.signin),
+    path('index', views.index, name='index'),
+    path('catalogoCuenta/', views.catalogoCuenta, name='catalogoCuenta'),
+    path('transacciones/', views.transaccion, name='transacciones'),
+    path('balanceComprobacion/', views.balanceComprobacion,name='balanceComprobacion'),
+    path('estadoResultados/', views.estadoResultados, name='estadoResultados'),
+    path('estadoCapital/', views.estadoCapital, name='estadoCapital'),
+    path('balanceGeneral/', views.balanceGeneral, name='balanceGeneral'),
+    path('signout/', views.signout, name='signout'),
 ]
--- inventario/urls.py
+++ inventario/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio),
-    path('adquisicionMateriaPrima/', views.adquisicionMateriaPrima),
-    path('ventaProducto/', views.ventaProducto),
-    path('inventario/', views.manejoInventario),
-    path('contratacion/', views.contratacion),
-    path('plantillaGeneralEmpleados/', views.plantillaGeneralEmpleados),     
+    path('adquisicionMateriaPrima/', views.adquisicionMateriaPrima, name='adquisicionMateriaPrima'),
+    path('ventaProducto/', views.ventaProducto, name='ventaProducto'),
+    path('inventario/', views.manejoInventario, name='inventario'),
+    path('contratacion/', views.contratacion, name='contratacion'),
+    path('plantillaGeneralEmpleados/', views.plantillaGeneralEmpleados, name='plantillaGeneralEmpleados'),     
 ]
