a1408be618adb7ac51c51b2acc0a9659dd9c00f4
RC22009ues <hec159357@gmail.com>
2024-11-04 23:39:56 -0600

    cambios

--- Contabilidad/views.py
+++ Contabilidad/views.py
@@ -1,10 +1,11 @@
+from django.db.models import Sum
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate,login,logout
 from .models import Cuenta, Periodo, Transaccion
-from django.db.models import Sum
+
 
 # Create your views here.
 @login_required(login_url='/signin/')
