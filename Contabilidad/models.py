5eb1ddd60e55dad1c6c28eb04ffd4a870e5b02e8
RC22009ues <hec159357@gmail.com>
2024-11-04 18:31:34 -0600

    cambios

--- Contabilidad/models.py
+++ Contabilidad/models.py
@@ -26,12 +26,31 @@ class Cuenta(models.Model):
 from django.db import models
 
 class Periodo(models.Model):
-    nombre = models.CharField(max_length=100)
-    fecha_inicio = models.DateField()
-    fecha_fin = models.DateField()
+    MESES = [
+        (1, "Enero"),
+        (2, "Febrero"),
+        (3, "Marzo"),
+        (4, "Abril"),
+        (5, "Mayo"),
+        (6, "Junio"),
+        (7, "Julio"),
+        (8, "Agosto"),
+        (9, "Septiembre"),
+        (10, "Octubre"),
+        (11, "Noviembre"),
+        (12, "Diciembre"),
+    ]
+
+    
+    mes = models.PositiveSmallIntegerField(choices=MESES, default=1)  # Valor por defecto para mes
+    ano = models.PositiveIntegerField(default=2024)  # Valor por defecto para año
 
     def __str__(self):
-        return f'{self.nombre}- {self.id}'
+        return f"{self.get_mes_display()} {self.ano}"
+
+    class Meta:
+        verbose_name_plural = "Periodos"
+        unique_together = ('mes', 'ano')  # Evita duplicados  # Evita que se dupliquen el mismo mes y año
 
 # transacciones
 class Transaccion(models.Model):
